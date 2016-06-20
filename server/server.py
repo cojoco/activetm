b7c136df4ddfc0cff4c39be26a3459cb247b48d6
cojoco <cojoco@users.noreply.github.com>
2016-06-20 16:48:18 -0600

    Work on one-dimensional Metadata map

--- server/server.py
+++ server/server.py
@@ -1,4 +1,5 @@
-"""Runs a user interface for the interactive anchor words algorithm"""
+#! /usr/bin/ENV python3
+"""Runs the Metadata Map"""
 
 import flask
 import os
@@ -9,7 +10,37 @@ import random
 
 APP = flask.Flask(__name__, static_url_path='')
 
+def get_user_dict_on_start():
+    """Loads user data"""
+    # This maintains state if the server crashes
+    try:
+        last_state = open('last_state.pickle', 'rb')
+    except IOError:
+        print('No last_state.pickle file, assuming no previous state')
+    else:
+        state = pickle.load(last_state)
+        print("Last state: " + str(state))
+        last_state.close()
+        return state['USER_DICT']
+    # but if the server is starting fresh, so does the user data
+    return {}
+
+
+###############################################################################
+# Everything in this block needs to be run at server startup
+# USER_DICT holds information for individual users
+USER_DICT = get_user_dict_on_start()
+LOCK = threading.Lock()
 RNG = random.Random()
+###############################################################################
+
+
+def save_state():
+    """Saves the state of the server to a pickle file"""
+    last_state = {}
+    last_state['USER_DICT'] = USER_DICT
+    print(USER_DICT)
+    pickle.dump(last_state, open('last_state.pickle', 'wb'))
 
 
 @APP.route('/')
@@ -24,12 +55,46 @@ def serve_ui():
     return flask.send_from_directory('static', 'onedimension.html')
 
 
+@APP.route('/end')
+def serve_end():
+    """Serves the end page, which gets rid of cookies"""
+    return flask.send_from_directory('static', 'end.html')
+
+
 @APP.route('/uuid')
 def get_uid():
     """Sends a UUID to the client"""
     uid = uuid.uuid4()
     data = {'id': uid}
+    with LOCK:
+        USER_DICT[str(uid)] = {
+            'current_doc': -1
+        }
+        save_state()
     return flask.jsonify(data)
+
+
+@APP.route('/labeldoc', methods=['POST'])
+def label_doc():
+    """Receives the label for the previously sent document"""
+    user_id = flask.request.headers.get('uuid')
+    doc_number = flask.request.values.get('doc_number')
+    label = flask.request.values.get('label')
+    print("doc_number: ", doc_number, " label: ", label)
+    return flask.jsonify(user_id=user_id)
+
+
+@APP.route('/getdoc')
+def get_doc():
+    """Gets the next document for this user"""
+    user_id = flask.request.headers.get('uuid')
+    doc_number = -1
+    document = ''
+    with LOCK:
+        if user_id in USER_DICT:
+            # do what we need to get the right document for this user
+            pass
+    return flask.jsonify(document=document, doc_number=doc_number)
 
 
 if __name__ == '__main__':
--- server/static/scripts/oneDScript.js
+++ server/static/scripts/oneDScript.js
@@ -1,11 +1,60 @@
 $(document).ready(function() {
-  $(".button").click(function() {
-    $("#testDoc").animate({left: '+=100px',
-                           top: '+=100px'})
-  })
+  /*This is just so I remember how to animate things
+  $("#testDoc").animate({left: '+=100px',
+                         top: '+=100px'})
+  */
 
   // Get info on the line's position to use to place the documents
   var minX = Number($("#lineBase").attr("x1"))
   var maxX = Number($("#lineBase").attr("x2"))
-  var lineY = Number($("#lineBase").attr("y1"))
+  var minY = Number($("#lineBase").attr("y1"))-30
+  var maxY = minY + 36
+
+  if (Cookies.get('uuid') === undefined) {
+    $.get('/uuid', function(data) {
+      Cookies.set('uuid', data['id'])
+    })
+  }
+
+  $("#docButton").click(function(ev) {
+    $.ajax({
+      url: '/getdoc',
+      headers: {'uuid': Cookies.get('uuid')},
+      success: function(data) {
+        Cookies.set('doc_number', data['doc_number'])
+        console.log(data)
+      }
+    })
+  })
+
+  $("#testDoc").draggable({
+    containment: 'parent',
+    cursor: 'move',
+    stop: docDragHandler
+  })
+
+  function docDragHandler( event, ui ) {
+    var offsetXPos = parseInt(ui.offset.left) - $(".container").offset().left
+    var offsetYPos = parseInt(ui.offset.top)
+    if (offsetXPos >= minX && offsetXPos < maxX &&
+        offsetYPos >= minY && offsetYPos < maxY) {
+      //Normalize the label to a value between 0 and 1 to send back
+      console.log("offsetXPos: " + offsetXPos)
+      console.log("minX: " + minX + " maxX: " + maxX)
+      var lineLeft = Number($("#lineBase").attr("x1"))
+      var lineRight = Number($("#lineBase").attr("x2"))
+      var label = (offsetXPos - lineLeft)/(lineRight - lineLeft)
+      $.ajax({
+        url: '/labeldoc',
+        method: 'POST',
+        headers: {'uuid': Cookies.get('uuid')},
+        data: {'doc_number': Cookies.get('doc_number'),
+               'label': label
+              },
+        success: function(data) {
+          console.log("labeled a document")
+        }
+      })
+    } 
+  }
 })

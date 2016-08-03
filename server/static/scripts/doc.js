9bb761041e0b5eb90fcbf952b64c8124416e4c03
cojoco <cojoco@users.noreply.github.com>
2016-08-03 12:20:35 -0600

    Found another way to represent the modal in Javascript, it might be more readable?

--- server/static/scripts/doc.js
+++ server/static/scripts/doc.js
@@ -35,39 +35,89 @@ Document.prototype = {
                                    .attr('data-target', '#' + modalId)
                                    .text('Select Document')
     //Create the modal itself
-    var modal = $('<div>').attr('id', modalId)
-                          .attr('class', 'modal fade')
-                          .attr('role', 'dialog')
-    var modalOuter = $('<div>').attr('class', 'modal-dialog')
-    var modalInner = $('<div>').attr('class', 'modal-content')
-    var modalTabs = $('<div>').attr('class', 'tab-content')
     var tab1Id = 'modal' + this.id + 'Tab1'
-    var modalTab1 = $('<div>').attr('id', tab1Id)
-                      .attr('class', 'tab-pane fade in active')
-                      .html('<p>This is the first tab</p>')
     var tab2Id = 'modal' + this.id + 'Tab2'
-    var modalTab2 = $('<div>').attr('id', tab2Id)
-                      .attr('class', 'tab-pane fade')
-                      .html('<p>This is the second tab</p>')
     var tab3Id = 'modal' + this.id + 'Tab3'
-    var modalTab3 = $('<div>').attr('id', tab3Id)
-                      .attr('class', 'tab-pane fade')
-                      .html('<p>This is the third tab</p>')
-    var modalGUITabs = $('<ul>').attr('class', 'nav nav-tabs')
-                      .append('<li class="active"><a data-toggle="tab" href="#' + tab1Id + '">Main</a></li>')
-                      .append('<li><a data-toggle="tab" href="#' + tab2Id + '">Text</a></li>')
-                      .append('<li><a data-toggle="tab" href="#' + tab3Id + '">Topics</a></li>')
-    var modalHeader = $('<div>').attr('class', 'modal-header')
-                        .html('<h4 class="modal-title">Title: ' + this.title +
-                              '</h4>')
-    var modalBody = $('<div>').attr('class', 'modal-body')
-                      .append(modalGUITabs)
-    //Put together the modal and its tab panes
-    modalTabs.append(modalTab1).append(modalTab2).append(modalTab3)
-    modalBody.append(modalTabs)
-    modalInner.append(modalHeader).append(modalBody)
-    modalOuter.append(modalInner)
-    modal.append(modalOuter)
+    var temp = $('<div>')
+    //Basically what I do is create a very long template, append it to temp
+    //  to get it parsed, then pull it out of temp and put it in the modals div
+    //I'm not sure whether this is better or worse than building it using
+    //  JQuery and .attr() below.
+    var tmpMod = '<div id="' + modalId + '" class="modal fade" ' +
+                   'role="dialog">' + 
+                   '<div class="modal-dialog">' + 
+                     '<div class="modal-content">' +
+                       '<div class="modal-header">' +
+                         '<h4 class="modal-header">' +
+                           'Title: ' + this.title +
+                         '</h4>' +
+                       '</div>' +
+                       '<div class="modal-body">' +
+                         '<ul class="nav nav-tabs">' + 
+                           '<li class="active">' +
+                             '<a data-toggle="tab" href="#' + tab1Id + '">' +
+                               'Main' +
+                             '</a>' +
+                           '</li>' +
+                           '<li>' +
+                             '<a data-toggle="tab" href="#' + tab2Id + '">' +
+                               'Text' +
+                             '</a>' +
+                           '</li>' +
+                           '<li>' +
+                             '<a data-toggle="tab" href="#' + tab3Id + '">' +
+                               'Topics' +
+                             '</a>' +
+                           '</li>' +
+                         '</ul>' +
+                         '<div class="tab-content">' +
+                           '<div id="' + tab1Id + '" class="tab-pane fade ' +
+                             'active in">' +
+                             '<p>This is the first tab ' + tab1Id + '</p>' +
+                           '</div>' +
+                           '<div id="' + tab2Id + '" class="tab-pane fade">' +
+                             '<p>This is the second tab ' + tab2Id + '</p>' +
+                           '</div>' +
+                           '<div id="' + tab3Id + '" class="tab-pane fade">' +
+                             '<p>This is the third tab ' + tab3Id + '</p>' +
+                           '</div>' +
+                         '</div>' +
+                       '</div>' +
+                     '</div>' +
+                   '</div>' +
+                 '</div>'
+    temp.html(tmpMod)
+    var modal = temp.first()
+//    var modal = $('<div>').attr('id', modalId)
+//                          .attr('class', 'modal fade')
+//                          .attr('role', 'dialog')
+//    var modalOuter = $('<div>').attr('class', 'modal-dialog')
+//    var modalInner = $('<div>').attr('class', 'modal-content')
+//    var modalTabs = $('<div>').attr('class', 'tab-content')
+//    var modalTab1 = $('<div>').attr('id', tab1Id)
+//                      .attr('class', 'tab-pane fade in active')
+//                      .html('<p>This is the first tab</p>')
+//    var modalTab2 = $('<div>').attr('id', tab2Id)
+//                      .attr('class', 'tab-pane fade')
+//                      .html('<p>This is the second tab</p>')
+//    var modalTab3 = $('<div>').attr('id', tab3Id)
+//                      .attr('class', 'tab-pane fade')
+//                      .html('<p>This is the third tab</p>')
+//    var modalGUITabs = $('<ul>').attr('class', 'nav nav-tabs')
+//                      .append('<li class="active"><a data-toggle="tab" href="#' + tab1Id + '">Main</a></li>')
+//                      .append('<li><a data-toggle="tab" href="#' + tab2Id + '">Text</a></li>')
+//                      .append('<li><a data-toggle="tab" href="#' + tab3Id + '">Topics</a></li>')
+//    var modalHeader = $('<div>').attr('class', 'modal-header')
+//                        .html('<h4 class="modal-title">Title: ' + this.title +
+//                              '</h4>')
+//    var modalBody = $('<div>').attr('class', 'modal-body')
+//                      .append(modalGUITabs)
+//    //Put together the modal and its tab panes
+//    modalTabs.append(modalTab1).append(modalTab2).append(modalTab3)
+//    modalBody.append(modalTabs)
+//    modalInner.append(modalHeader).append(modalBody)
+//    modalOuter.append(modalInner)
+//    modal.append(modalOuter)
     //Stick the modal and tabs in the DOM
     $("#modals").append(modal)
     //Return a newly created paragraph

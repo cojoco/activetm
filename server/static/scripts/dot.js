5d4f93c207a5b1c4711e5ff14267083f2479a37c
cojoco <cojoco@users.noreply.github.com>
2016-08-05 12:12:39 -0600

    Got topics sent with each doc, got test pie chart displaying in modal

--- server/static/scripts/dot.js
+++ server/static/scripts/dot.js
@@ -1,5 +1,10 @@
 //This class holds the dot id, position and corresponding document number
 
+//Creates an SVG element in a way JQuery can work with it
+function makeSVG(tag) {
+  return document.createElementNS('http://www.w3.org/2000/svg', tag)
+}
+
 function Dot(docNumber, posX, posY) {
   this.id = 'dot' + docNumber //ID of the dot in the DOM
   this.docNumber = docNumber //Number of the corresponding document
@@ -12,20 +17,15 @@ Dot.prototype = {
 
   //Creates a SVG dot to stick on the map
   toSVG: function toSVG() {
-    return $(svg('circle')).attr('id', this.id)
-                           .attr('class', 'dot')
-                           .attr('cx', this.posX + '%')
-                           .attr('cy', this.posY + '%')
-                           .attr('r', '2%')
-                           .attr('stroke', 'gray')
-                           .attr('stroke-width', 1)
-                           .attr('fill', 'gray')
-                           .attr('stroke-opacity', '0.2')
-                           .attr('fill-opacity', '0.2')
+    return $(makeSVG('circle')).attr('id', this.id)
+                               .attr('class', 'dot')
+                               .attr('cx', this.posX + '%')
+                               .attr('cy', this.posY + '%')
+                               .attr('r', '2%')
+                               .attr('stroke', 'gray')
+                               .attr('stroke-width', 1)
+                               .attr('fill', 'gray')
+                               .attr('stroke-opacity', '0.2')
+                               .attr('fill-opacity', '0.2')
   }
-}
-
-//Creates an SVG element in a way JQuery can work with it
-function svg(tag) {
-  return document.createElementNS('http://www.w3.org/2000/svg', tag)
 }
